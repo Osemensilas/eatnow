fbc395c557ba38eb632775ed1e33efc6a95c9be1
unknown <osemensilas@gmail.com>
2025-08-25 20:36:01 +0100

    New Commit

--- pages/help.jsx
+++ pages/help.jsx
@@ -1,8 +1,56 @@
 import FAQs from "@/components/FAQs";
+import styles from "../styles/faqs.module.css";
 
 const Help = () => {
     return ( 
         <>
+        <section id={styles.hero}>
+            <div className={styles.heroContent}>
+                <header>
+                    <h2>How Can We Help You?</h2>
+                    <div className={styles.headerLine}></div>
+                    <p>We are here for you. If you have any question or anything is now working, you can answers here.</p>
+                </header>
+                <div className={styles.fagContent}>
+                    <div className={styles.faqCard}>
+                        <div className={styles.faqCardTop}>
+                            <img src="/" alt="" />
+                        </div>
+                        <div className={styles.faqCardBottom}>
+                            <p>How to register on EatNow</p>
+                            <div className={styles.readTime}>5 min read</div>
+                        </div>
+                    </div>
+                    <div className={styles.faqCard}>
+                        <div className={styles.faqCardTop}>
+                            <img src="/" alt="" />
+                        </div>
+                        <div className={styles.faqCardBottom}>
+                            <p>How to register on EatNow</p>
+                            <div className={styles.readTime}>5 min read</div>
+                        </div>
+                    </div>
+                    <div className={styles.faqCard}>
+                        <div className={styles.faqCardTop}>
+                            <img src="/" alt="" />
+                        </div>
+                        <div className={styles.faqCardBottom}>
+                            <p>How to register on EatNow</p>
+                            <div className={styles.readTime}>5 min read</div>
+                        </div>
+                    </div>
+                    <div className={styles.faqCard}>
+                        <div className={styles.faqCardTop}>
+                            <img src="/" alt="" />
+                        </div>
+                        <div className={styles.faqCardBottom}>
+                            <p>How to register on EatNow</p>
+                            <div className={styles.readTime}>5 min read</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <FAQs />
         </>
      );
